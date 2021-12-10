b8a5e21aaddc82958829ddea57513f232c1263dd
mike ¯\_(ツ)_/¯ <mikerapin@gmail.com>
2021-12-09 22:23:25 -0500

    Added linters, json, dev quality pieces
    - Added eslint, prettier, vscode extensions for dev quality purposes
    - Linted the project
    - Updated node server to serve up JSON in chunks
    - Added a few other packages for future use (bad practice, I know)
    - Created basic UI with type to autocomplete functions

--- node-server/app.js
+++ node-server/app.js
@@ -1,14 +1,46 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3001;
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// This is simply to unblock the two packages running on localhost and the CORS issues caused by default
+// Long term, this would be a very strict policy to prevent XSS
+app.use(cors());
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`This is not a great example of a server.`);
+/**
+ * Ideally, this data would be in a database or some sort of storage that might be easier to search an index
+ * Searching JSON can be tedious at scale
+ */
+let rawdata = fs.readFileSync('./node-server/KITS_SHIPPING_DATA.json', { flag: 'r' });
+let kitData = JSON.parse(rawdata);
+
+app.get('/kit/:id', (req, res) => {
+    const kitId = req.params.id.replace(/\D/g, '');
+
+    const kitsFound = [];
+    const labels = kitData.map(kit => kit['label_id']);
+    let start = 0;
+    let end = 9;
+
+    // This isn't necessarily efficient, but it ensures that we don't need to iterate through the whole set
+    // of data to find the labels we need
+    while (kitsFound.length <= 10 && end < labels.length) {
+        labels.slice(start, end).forEach((label, index) => {
+            const cleanedLabel = label.replace(/\D/g, '');
+            if (cleanedLabel.startsWith(kitId)) {
+                kitsFound.push(kitData[start + index]);
+            }
+        });
+        start += 10;
+        end += 10;
+    }
+
+    res.type('json'); // => 'application/json'
+    res.type('application/json'); // => 'application/json'
+    res.send(kitsFound);
 });
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,102 @@
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="container">
-      <header className="header">
-        <h1>Biobot Kit Search</h1>
-      </header>
-      <div className="row">
-        <div className="six columns">
-          <input type="text" name="search" onKeyDown={() => console.log('typing')} className="search-box twelve columns" />
+import debounce from 'lodash/debounce';
+
+type KitData = { id: number; label_id: string; shipping_tracking_code: string };
+type KitDataList = KitData[];
+
+const fetchFromServer = (value: string) => {
+    return fetch(`http://localhost:3001/kit/${value}`).then(res => {
+        return res.json();
+    });
+};
+
+/**
+ *
+ * @param label
+ * @param search
+ * @returns
+ */
+const matchLabelToSearch = (label: string, search: string): React.ReactElement => {
+    let firstHalf = '';
+    let labelIterator = 0;
+    for (let i = 0; i < search.length && labelIterator < label.length; i++) {
+        if (search[i] !== label[labelIterator]) {
+            i--;
+        }
+        firstHalf += label[labelIterator];
+        labelIterator++;
+    }
+
+    return (
+        <>
+            <span className="search-highlight">{firstHalf}</span>
+            <span>{label.slice(labelIterator)}</span>
+        </>
+    );
+};
+
+const App = (): React.ReactElement => {
+    const [searchData, setSearchData] = useState<KitDataList>([]);
+    const [lastSearchedValue, setLastSearchedValue] = useState<string>('');
+    const typeHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const currentValue = e.currentTarget.value.trim();
+        if (currentValue !== lastSearchedValue) {
+            setLastSearchedValue(currentValue);
+            if (currentValue.length > 1 && parseInt(currentValue, 10) > 0) {
+                fetchFromServer(currentValue).then(searchValues => setSearchData(searchValues));
+            } else {
+                setSearchData([]);
+            }
+        }
+    };
+    return (
+        <div className="container">
+            <header className="header">
+                <h1>Biobot Kit Search</h1>
+            </header>
+            <div className="row">
+                <div className="four columns">
+                    <input
+                        type="text"
+                        name="search"
+                        onKeyUp={typeHandler}
+                        className="search-box twelve columns"
+                        placeholder="Type your Label ID to begin searching"
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <table className="six columns">
+                    <thead>
+                        <tr>
+                            <th>Label</th>
+                            <th>FedEx Tracking Number</th>
+                        </tr>
+                    </thead>
+                    {searchData.length > 0 && (
+                        <tbody>
+                            {searchData.map((kit: KitData) => (
+                                <tr key={kit.id}>
+                                    <td>{matchLabelToSearch(kit.label_id, lastSearchedValue)}</td>
+                                    <td>
+                                        <a
+                                            href={`https://www.fedex.com/fedextrack/?tracknumbers=${kit.shipping_tracking_code}`}
+                                            rel="noreferrer"
+                                            target="_blank">
+                                            {kit.shipping_tracking_code}
+                                        </a>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    )}
+                </table>
+            </div>
         </div>
-        <div className="two columns">
-          <button type="button" onClick={() => console.log('search!')}>
-            Search
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
 export default App;
